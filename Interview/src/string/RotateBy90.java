7084160bd3576879db8f3edc44b4d33b5625945a
GTMDEV <140165729+GTMDEV@users.noreply.github.com>
2023-07-24 19:43:56 +0530

    90 degree rotation

---
+++ Interview/src/string/RotateBy90.java
@@ -0,0 +1,55 @@
+package string;
+
+/*
+ *  1, 2, 3 
+ *  4, 5, 6
+ *  7, 8, 9
+ *  
+ *   
+ *   7 4 1 
+     8 5 2 
+     9 6 3 
+
+ */
+public class RotateBy90 {
+
+	public static void rotateMatrix(int[][] matrix) {
+		if (matrix.length == 0 || matrix.length != matrix[0].length) {
+			return;
+		}
+		int n = matrix.length;
+		for (int layer = 0; layer < n / 2; layer++) {
+			int first = layer;
+			int last = n - 1 - layer;
+
+			for (int i = first; i < last; i++) {
+				int offset = i - first;
+				int top = matrix[first][i];
+
+				matrix[first][i] = matrix[last - offset][first];
+
+				matrix[last - offset][first] = matrix[last][last - offset];
+
+				matrix[last][last - offset] = matrix[i][last];
+
+				matrix[i][last] = top;
+			}
+
+		}
+
+	}
+
+	public static void main(String[] args) {
+		int[][] matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+
+		rotateMatrix(matrix);
+
+		// Print the rotated matrix
+		for (int[] row : matrix) {
+			for (int num : row) {
+				System.out.print(num + " ");
+			}
+			System.out.println();
+		}
+	}
+}
